7e01b191a53b90175b822bcf74ec8b41500d3d48
Angelica <a.cortes.godoy@gmail.com>
2022-07-24 23:38:12 -0400

    edit css

--- assets/js/calcular.js
+++ assets/js/calcular.js
@@ -1,9 +1,29 @@
+ele = document.querySelector(".propiedades")
+for (let valor of propiedadesJSON)
+    ele.innerHTML +=  `
+    <div class="propiedades">
+        <div class="propiedad">
+            <div class="img" style="background-image: url('${valor.src}')"></div>
+            <section>
+                <h5>${valor.name}</h5>
+                <div class="d-flex justify-content-between">
+                    <p>Cuartos: ${valor.rooms}</p>
+                    <p>Metros: ${valor.m}</p>
+                </div>
+                <p class="my-3">${valor.description}</p>
+                <button class="btn btn-info ">Ver más</button>
+            </section>
+        </div>
+    </div>
+  </section>
+  `
 function calcular() {
     let cuartos = document.querySelector("#cuartos").value;
     let metrosDesde = document.querySelector("#desde").value;
     let metrosHasta = document.querySelector("#hasta").value;
     let parrafo = document.querySelector('#resultado');
     cuenta = 0
+    
     if (cuartos == "" || metrosDesde == "" || metrosHasta == "") {
       alert("Faltan campos por llenar");
     }
